617783b5fc9b75e7914c2ad6a106549b5ff55b14
wawanseptiyawan45 <wawanseptiyawan45@gmail.com>
2016-11-22 15:16:34 +0700

    12141383 - line 15 adalah nma class tebakAngka dn template pada packge com.septiyawan.app12141383 			line 17 - 21 deklarasi atribut variableyang digunakan pada tebakAngka dengan nama untuk memasukkan nama yang diinginkan yang nantinya dideklarasikan dibwah, level,giiran type data bekerja pada streaam, sedangkan type data short yang mengisikan tebak, banyak digunakan untuk vriable yang memiliki 16-bit.
    12141383 - line 22-23 int masukkan variable yang nanti akn dimasukkan pada program tebakAngka dengan isi angka,randomm dan nilai
    			line 24-25 type data untuk hasil keseluruhan variable
    
    12141383 - line 26-27 int[] history = new int; di gunakan untuk memasukan riwayat pada variabel history, public int Level() { return level*100; pada type data ini digunakan untuk level yaitu memasukkan tebakan
    
    12141383 - line 31-33 public void hasilmenebak, atribut ini untuk akses dri mana pun khususnya untuk type data sisa dan banyak.
    
    12141383 - line 36-38 script ini menampilkan jumlah tebakan yang diinginkan dan jika tebkn salah maka if (tebak<angka) menyatakan jika jumlah tebak dan angka terlalu kecil maka akan muncul perintah yang akan muncul tebakan anda terlalu kecil
    
    12141383 - line 40-45 variabel ini keblikan dari vribel diatas yaitu tebakan anda terlalu besar, maka dan jika behasil menebak maka akan muncul selamat anda berhasil menebak sebanyak, dengan memanggil banyak tebakan.
    
    12141383 - line 47-51 variabel if dengan menampilkan sisa tebakan memanggil type data sisa,tebak dan angka seperti pada line 48 dengan keterangan anda mempunyai kesempatan dan memanggil type data sisa, maka jika tidak berhasil menebak perintah yang kluar adalah Game Over seperti pada script 50-51.
    
    12141383 - line 55-57 memanggil varible Nilai/skor dengan type data banyak,nilai dan level, pada line 56-57 perulangan menampilkan skor untuk banyaknya tebakan, jika tebakan salah maka akan di tampilkan script berikutnya
    
    12141383 - line 59-60 menampilkan banyak tebakan 1 maka akan dikalikan 70
    
    12141383 - line 62,63 menampilkan banyak tebakan 2 maka akan dikalikan 50
    
    12141383 - line 65,66 menampilkan banyak tebakan 3 maka akan dikalikan 30
    
    12141383 - line 68,69 menampilkan banyak tebakan 4 maka akan dikalikan 0
    
    12141383 - line 71-72 menmpilkan skor keseluruhan dari banyknya tebakan.
    
    12141383 - line 76-77 history/riwayat dari tebak angka tersebut dengan menampilkan total
    
    12141383 - line 80-81 menampilkan total skor dari tebakan.
    
    12141383 - line 84-88 type data reset menampilkan variable level,giliran,banyk,dan total

---
+++ src/com/septiyawan/app12141383/tebakAngka.java
@@ -0,0 +1,141 @@
+/*
+ * Septiyawan Nifthoh Syafi'i
+ * 12141383
+ */
+package com.septiyawan.app12141383;
+
+/**
+ *
+ * @author Wawan
+ */
+
+import java.util.Scanner;
+
+
+public class tebakAngka {
+   
+    String nama;
+    byte level = 1;
+    byte giliran = 5, sisa;
+    short tebak;
+    short banyak = 0;
+    int angka = 5;
+    int random, nilai = 0;
+    char jwb, pilih;
+    int total = 0, x=0;
+    int[] history = new int[5];
+    public int Level(){
+        return level*100;
+    }
+    
+    public void hasilmenebak() {
+      sisa--;
+      banyak++;
+      
+      
+        System.out.println("Tebakan anda : " + tebak);
+        if (tebak<angka){
+            System.out.print("Tebakan Anda telalu kecil! ");
+        }
+        if (tebak>angka){
+            System.out.println("Tebakan Anda telalu besar!");
+        }
+        if (tebak==angka){
+            System.out.print("Selamat Anda berhasil menebak sebanyak " + banyak + " kali tebakan");
+        }
+        
+        if (sisa>0&&tebak!=angka){
+            System.out.println("Anda mempunyai kesempatan " + sisa + " kali lagi.");
+        }
+        if (sisa<=0) {
+            System.out.println("Game Over");
+        }
+    }
+    
+    public void Nilai() {
+        if (banyak>0){
+            nilai = 100*level;
+        }
+        if (banyak>1){
+            nilai = 70*level;
+        }
+        if (banyak>2){
+            nilai = 50*level;
+        }
+        if (banyak>3){
+            nilai = 30*level;
+        }
+        if (banyak>4){
+            nilai = 0*level;
+        }
+        System.out.println();
+        System.out.println(nama + "\t: " + nilai);
+        total=total+nilai;
+    }
+    
+    public void History(){
+        history [x] = total;
+    }
+    
+    public void totalNilai(){
+        System.out.println("Total score anda : " + total);        
+    }
+    
+    public void Reset(){        
+    level = 1;
+    giliran = 5;
+    banyak=0;
+    total=0;
+    }
+    
+    public static void main(String[] args) {
+        tebakAngka main = new tebakAngka();
+        System.err.println("Game Tebk Angka");
+        System.err.println("Septiyawan Nifthoh Syafi'i");
+        System.err.println("12141383");
+        System.err.println("================================");
+        
+        Scanner inputString = new Scanner(System.in);
+        Scanner inputAngka = new Scanner(System.in);
+        
+        do{
+        System.out.println("Masukkan nama Anda untuk memulai game atau tekan 'Y' untuk keluar, kemudian tekan enter : ");
+        main.nama=inputString.nextLine();
+        System.out.println("Selamat Datang." + main.nama);
+        
+          if ("T".equals(main.nama)){
+              break;
+          }
+          else{
+            do {
+            main.random = (short) (Math.random()*main.Level());
+            System.out.println("# Level " + main.level);  
+            System.out.println("Selamat datang " + main.nama);  
+            System.out.println("Silakan tebak angka antara 1 s/d " + main.Level());    
+            System.out.println("Anda memiliki kesempatan menebak " + main.giliran + " kali"); 
+            main.sisa=main.giliran;
+            
+            do{
+                System.out.println();   
+                System.out.println("Masukkan Tebakan : "); 
+                main.tebak = inputAngka.nextShort();
+                main.hasilmenebak();
+            }while(main.tebak!=main.random&&main.sisa>0);
+            
+            
+            if (main.tebak==main.random){
+                main.Nilai();
+                System.out.println("Apakah anda ingin melanjutkan ke level selanjutnya ? (y/t)");
+                main.jwb=inputString.nextLine().charAt(0);
+                main.level++;
+                main.banyak=0;
+            }
+            else {     break;}
+            }while(main.jwb=='y');
+            main.totalNilai();
+            main.Reset();
+          }
+        }while(!"T".equals(main.nama)); 
+        
+    }        
+}
